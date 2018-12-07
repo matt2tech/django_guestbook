b9a9bc95e7f0a49d2b4da65a7541a228841794d6
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-07 10:04:16 -0600

    Added a view for homepage

--- app/views.py
+++ app/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from . import forms
+from app import models
 
 # Create your views here.
 class Note(View):
     def get(self, request):
-        return render(request, 'app/note.html', {'form': forms.NoteForm()})
+        return render(request, 'note.html', {'form': forms.NoteForm()})
 
     def post(self, request):
         form = forms.NoteForm(data=request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             message = form.cleaned_data['message']
-            return render(request, 'app/note.html', {'name': name, 'message': message, 'form': form})
+            models.NoteModel.submitted({'name': name, 'message': message})
         else:
-            return render(request, 'app/note.html', {'form': form})
+            return render(request, 'note.html', {'form': form})
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'homepage.html')
