f7d1b29d5541c0a4f4c57808e50c14a3d6eeb363
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-07 09:03:14 -0600

    Added class Note for guests to leave notes

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from . import forms
 
 # Create your views here.
+class Note(View):
+    def get(self, request):
+        return render(request, 'app/note.html', {'form': forms.NoteForm()})
+
+    def post(self, request):
+        form = forms.NoteForm(data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            message = form.cleaned_data['message']
+            return render(request, 'app/note.html', {'name': name, 'message': message, 'form': form})
+        else:
+            return render(request, 'app/note.html', {'form': form})
