9d150ecc4e70f5fa164046d8188ee222559c2e71
matt2tech <mlipsey@basecampcodingacademy.org>
2018-12-07 11:03:49 -0600

    Added NoteModel which sends data from frontend to database

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class NoteModel(models.Model):
+    name = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    @staticmethod
+    def submitted(dict):
+        NoteModel(name=dict['name'], message=dict['message']).save()
